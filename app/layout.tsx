7a538182f8e8d80020b5dad27a5e0a3d39d06e7a
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-05-05 17:48:57 +0700

    Version 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
+import { SessionProvider } from "next-auth/react";
+import NAP from "@/components/NAP";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +18,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <NAP>
+      <html lang="en">
+        <body className={inter.className}>
+          <Navbar></Navbar>
+          {children}
+        </body>
+      </html>
+    </NAP>
   );
 }
